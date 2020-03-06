489c737d3d3a9c32fb42c5bdfb212e68da17a65f
aartvanempel <aartvanempel@Aarts-Macbook.local>
2020-03-06 22:56:57 +0100

    show amount after debounce

--- src/features/monsters/Monster.js
+++ src/features/monsters/Monster.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { useDebouncedCallback } from 'use-debounce';
 import styles from './Monster.module.scss'
 import {
     decrement,
@@ -9,33 +10,74 @@ import {
 } from './monsterSlice';
 
 export default ({ monster }) => {
+    const [hpDiff, setHpDiff] = useState(0)
+    const [value, setValue] = useState(0)
+    const [showHpDiff, setShowHpDiff] = useState(false)
     const dispatch = useDispatch()
     const isDead = monster.currentHp === 0
 
+    const [debouncedCallback] = useDebouncedCallback(() => {
+        setShowHpDiff(false)
+        monster.currentHp === 0 ? setValue(0) : setValue(hpDiff)
+        setHpDiff(0)
+        setShowHpDiff(true)
+    }, 700)
+
     return (
-        <div className={`${isDead ? styles.isDead : styles.container}`}>
+        <div className={isDead ? styles.isDead : styles.container}>
             <div className={styles.infoContainer}>
                 <div className={styles.nameContainer}>
-                    <span style={{backgroundColor: monster.color}}/>
+                    <span style={{ backgroundColor: monster.color }} />
                     <h2>{monster.name}</h2>
                 </div>
                 <div className={styles.hpContainer}>
+                    {
+                        value !== 0 && monster.currentHp !== 0 && showHpDiff && (
+                            <span className={styles.hpDiff}>
+                                {value > 0 ? `+${value}` : `${value}`}
+                            </span>
+                        )
+                    }
                     <span className={styles.currentHp}>{monster.currentHp}</span>
                     <span className={styles.maxHp}>/{monster.maxHp}</span>
                 </div>
             </div>
             <div className={styles.buttonContainer}>
                 <div>
-                    <button onClick={() => dispatch(decrementByAmount({ id: monster.id }))}>-10</button>
+                    <button onClick={() => {
+                        dispatch(decrementByAmount({ id: monster.id }))
+                        setHpDiff(prev => prev - 10)
+                        debouncedCallback()
+                    }}>
+                        -10
+                    </button>
                 </div>
                 <div>
-                    <button onClick={() => dispatch(decrement({ id: monster.id }))}>-1</button>
+                    <button onClick={() => {
+                        dispatch(decrement({ id: monster.id }))
+                        setHpDiff(prev => prev - 1)
+                        debouncedCallback()
+                    }}>
+                        -1
+                    </button>
                 </div>
                 <div>
-                    <button onClick={() => dispatch(increment({ id: monster.id }))}>+1</button>
+                    <button onClick={() => {
+                        dispatch(increment({ id: monster.id }))
+                        setHpDiff(prev => prev + 1)
+                        debouncedCallback()
+                    }}>
+                        +1
+                    </button>
                 </div>
                 <div>
-                    <button onClick={() => dispatch(incrementByAmount({ id: monster.id }))}>+10</button>
+                    <button onClick={() => {
+                        dispatch(incrementByAmount({ id: monster.id }))
+                        setHpDiff(prev => prev + 10)
+                        debouncedCallback()
+                    }}>
+                        +10
+                    </button>
                 </div>
             </div>
         </div>
