d8a496927f8e26d013fff20374e993e19e3faa32
aartvanempel <aartvanempel@Aarts-Macbook.local>
2020-03-09 10:37:08 +0100

    new build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hitpoint-calculator/precache-manifest.d407af04043c42fbb86afa0b73dda699.js"
+  "/hitpoint-calculator/precache-manifest.342e93745d093499841d63f48225b31f.js"
 );
 
 self.addEventListener('message', (event) => {
